77283b441036a1b53eb37eaaf80865972f405968
satishreddy <satishreddysr777@gmail.com>
2024-01-19 17:43:41 -0500

    Configure frontend base bash file

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,6 @@
 dnf install nginx -y
 
-systemctl enable nginx
-systemctl start nginx
+cp expense.conf /etc/nginx/default.d/expense.conf
 
 rm -rf /usr/share/nginx/html/*
 
@@ -10,6 +9,5 @@ curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zi
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-cp expense.conf /etc/nginx/default.d/expense.conf
-
+systemctl enable nginx
 systemctl restart nginx
