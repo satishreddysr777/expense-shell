ffb39e645e19f34fd6011c6e5a99bbbfd92a96bb
satishreddy <satishreddysr777@gmail.com>
2024-01-19 17:46:43 -0500

    Configure mysql base bash file

--- frontend.sh
+++ frontend.sh
@@ -6,6 +6,7 @@ rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
+# shellcheck disable=SC2164
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
