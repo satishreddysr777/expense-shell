14081100e873e1ca7d9213bd907c614678c02c89
satishreddy <satishreddysr777@gmail.com>
2024-01-20 15:45:21 -0500

    Shell Code

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,33 @@
-dnf install nginx -y
+log_file=/tmp/expense.log
 
-cp expense.conf /etc/nginx/default.d/expense.conf
+Head() {
+  echo -e "\e[35m$1\e[0m"
+}
 
-rm -rf /usr/share/nginx/html/*
 
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+Head "Installing Nginx"
+dnf install nginx -y &>> $log_file
+echo $?
 
+Head "Copying expense config file"
+cp expense.conf /etc/nginx/default.d/expense.conf &>> $log_file
+echo $?
+
+Head "Removing Old/Default content"
+rm -rf /usr/share/nginx/html/* &>> $log_file
+echo $?
+
+Head "Download frontend application content"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>> $log_file
+echo $?
+
+Head "Extracting application content"
 # shellcheck disable=SC2164
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html &>> $log_file
+unzip /tmp/frontend.zip &>> $log_file
+echo $?
 
-systemctl enable nginx
-systemctl restart nginx
+Head "Start Nginx service"
+systemctl enable nginx &>> $log_file
+systemctl restart nginx &>> $log_file
+echo $?
