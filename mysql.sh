22f1d612a1366e8412f78ec5e2635d404b2de128
satishreddy <satishreddysr777@gmail.com>
2024-01-20 14:27:16 -0500

    Added echo statements

--- mysql.sh
+++ mysql.sh
@@ -1,12 +1,18 @@
+log_file=/tmp/mysql_log.log
 MYSQL_PASSWORD=$1
 
-dnf module disable mysql -y
+echo -e "\e[35mDisable default version of MySql\e[0m"
+dnf module disable mysql -y &>> $log_file
 
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo -e "\e[35mCopying mysql Repo file into yum repos\e[0m"
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>> $log_file
 
-dnf install mysql-community-server -y
+echo -e "\e[35mInstall mysql community server\e[0m"
+dnf install mysql-community-server -y &>> $log_file
 
-systemctl enable mysqld
-systemctl start mysqld
+echo -e "\e[35mEnabling mysqlD and start mysql\e[0m"
+systemctl enable mysqld &>> $log_file
+systemctl start mysqld &>> $log_file
 
-mysql_secure_installation --set-root-pass "$MYSQL_PASSWORD"
+echo -e "\e[35mSetting password to mysql\e[0m"
+mysql_secure_installation --set-root-pass "$MYSQL_PASSWORD" &>> $log_file
